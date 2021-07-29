c93a13cf52861036c18447ae9b261b1d11b14eaa
EC2 Default User <ec2-user@ip-172-31-78-229.ec2.internal>
2021-07-29 16:28:41 +0000

    added ngxFoundation

--- s3-angular-website/website/src/app/app.module.ts
+++ s3-angular-website/website/src/app/app.module.ts
@@ -3,6 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import {
+  AccordionModule,
+  AlertModule,        // Foundation Callouts
+  ButtonsModule,
+  CarouselModule,     // Foundation Orbit
+  CollapseModule,
+  BsDatepickerModule,
+  BsDropdownModule,   // Foundation Dropdown Menus and Dropdown Panes
+  ModalModule,        // Foundation Reveal
+  OffcanvasModule,
+  PaginationModule,
+  ProgressbarModule,
+  RatingModule,
+  SortableModule,
+  TabsModule,
+  TimepickerModule,
+  TooltipModule,
+  TypeaheadModule,
+} from 'ngx-foundation';
+
+
+
 import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
